62d4d32ce10fa1f658f8810378fe35d1a75205ef
Eben Quenneville <student.equenneville@gmail.com>
2023-08-01 10:22:54 -0400

    april tag testing

---
+++ april_tag_render.py
@@ -0,0 +1,68 @@
+import numpy as np
+from dt_apriltags import Detector
+import matplotlib.pyplot as plt
+import cv2
+import april_tags
+import time
+from pid import *
+
+cameraMatrix = np.array([ 1060.71, 0, 960, 0, 1060.71, 540, 0, 0, 1]).reshape((3,3))
+
+camera_params = ( cameraMatrix[0,0], cameraMatrix[1,1], cameraMatrix[0,2], cameraMatrix[1,2] )
+
+at_detector = Detector(families='tag36h11',
+                       nthreads=1,
+                       quad_decimate=1.0,
+                       quad_sigma=0.0,
+                       refine_edges=1,
+                       decode_sharpening=0.25,
+                       debug=0)
+
+if __name__ == "__main__":
+    # The video writer
+    cap = cv2.VideoCapture("April_Tag_Test.mkv")
+    ret, frame1 = cap.read()
+
+    height, width, layers = frame1.shape
+    size = (width, height)
+
+    out = cv2.VideoWriter("april_tag_render.mp4", cv2.VideoWriter_fourcc(*"mp4v"), 30, size)
+    fourcc = cv2.VideoWriter_fourcc(*"XVID")
+
+    count = 0  # the amount of frames that have been read
+    # create PID objects, no idea what the right values are
+    horizontal_pid = PID(0.1, 0, 0, 100)
+    vertical_pid = PID(0.1, 0, 0, 100)
+
+    # Video reading loop
+    while ret:
+        ret, frame = cap.read()
+        if not ret:
+            break
+
+        if count > 50:
+            break
+
+        print(f"now on frame {count}...")
+
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+        tags = at_detector.detect(
+            gray, estimate_tag_pose=True, camera_params=camera_params, tag_size=True
+        )
+
+        if len(tags) > 0:
+            positions = april_tags.get_positions(tags)
+            errors = april_tags.error_relative_to_center(
+                positions, frame.shape[0], frame.shape[1]
+            )
+            outputs = april_tags.output_from_tags(errors, horizontal_pid, vertical_pid)
+            frame = april_tags.draw_outputs(frame, outputs, tags)
+            frame = april_tags.render_tags(tags, frame)
+
+        out.write(frame)
+        count += 1
+
+cap.release()
+out.release()
+print("Finished rendering the video.")
--- april_tags.py
+++ april_tags.py
@@ -1,5 +1,7 @@
 from dt_apriltags import Detector
 import numpy as np
+import cv2
+from pid import *
 
 def get_tags(img) -> list:
     """Gets a list of tags from an image.
@@ -10,17 +12,24 @@ def get_tags(img) -> list:
     Returns:
         list: the list of tags found in the image
     """
-    cameraMatrix = np.array([ 1060.71, 0, 960, 0, 1060.71, 540, 0, 0, 1]).reshape((3,3))
+    cameraMatrix = np.array([1060.71, 0, 960, 0, 1060.71, 540, 0, 0, 1]).reshape((3, 3))
 
-    camera_params = ( cameraMatrix[0,0], cameraMatrix[1,1], cameraMatrix[0,2], cameraMatrix[1,2] )
-    at_detector = Detector(families='tag36h11',
-                       nthreads=1,
-                       quad_decimate=1.0,
-                       quad_sigma=0.0,
-                       refine_edges=1,
-                       decode_sharpening=0.25,
-                       debug=0)
-    
+    camera_params = (
+        cameraMatrix[0, 0],
+        cameraMatrix[1, 1],
+        cameraMatrix[0, 2],
+        cameraMatrix[1, 2],
+    )
+    at_detector = Detector(
+        families="tag36h11",
+        nthreads=1,
+        quad_decimate=1.0,
+        quad_sigma=0.0,
+        refine_edges=1,
+        decode_sharpening=0.25,
+        debug=0,
+    )
+
     tags = at_detector.detect(img, True, camera_params=camera_params, tag_size=True)
     return tags
 
@@ -36,7 +45,10 @@ def get_positions(tags: list) -> list[tuple[float, float, int]]:
     """
     return [[tag.center[0], tag.center[1], tag.tag_id] for tag in tags]
 
-def error_relative_to_center(centers: list[tuple[float, float, int]], width: int, height: int) -> list[tuple[float, float, int]]:
+
+def error_relative_to_center(
+    centers: list[tuple[float, float, int]], width: int, height: int
+) -> list[tuple[float, float, int]]:
     """The error relative to the center of the image. Used for PID.
 
     Args:
@@ -47,6 +59,60 @@ def error_relative_to_center(centers: list[tuple[float, float, int]], width: int
     Returns:
         list[tuple[float, float, int]]: the list of error values for each tag
     """
-    x_center = height/2
-    y_center = width/2
-    return [[center[0] - x_center, y_center - center[1], center[2]] for center in centers]
+    x_center = height / 2
+    y_center = width / 2
+    return [
+        [center[0] - x_center, y_center - center[1], center[2]] for center in centers
+    ]
+
+
+def output_from_tags(errors, horizontal_pid: PID, vertical_pid: PID) -> tuple[list[float], list[float]]:
+    horizontal_output = [horizontal_pid.update(error[0]) for error in errors]
+    vertical_output = [vertical_pid.update(error[1]) for error in errors]
+    return (horizontal_output, vertical_output)
+
+def draw_outputs(img, outputs: tuple[list[float], list[float]], tags):
+    for i in range(len(outputs) - 1):
+        tag = tags[i]
+        horizontal = outputs[0][i]
+        vertical = outputs[1][i]
+        cv2.putText(
+            img,
+            f"Horizontal: {horizontal * 100:.2f}%",
+            org=(int(tag.corners[0][0]), int(tag.corners[0][1])),
+            fontFace=cv2.FONT_HERSHEY_SIMPLEX,
+            fontScale=1,
+            color=(0, 255, 0),
+        )
+        cv2.putText(
+            img,
+            f"Vertical: {vertical * 100:.2f}%",
+            org=(int(tag.corners[3][0]), int(tag.corners[3][1])),
+            fontFace=cv2.FONT_HERSHEY_SIMPLEX,
+            fontScale=1,
+            color=(0, 255, 0),
+        )
+    return img
+
+
+def render_tags(tags, img):
+    """Renders a list of tags into the image.
+
+    Args:
+        tags (list[tag]): the list of tags to render
+        img (_type_): the image to render tags on
+
+    Returns:
+        img: the image with tags drawn onto it
+    """
+    for tag in tags:
+        for idx in range(len(tag.corners)):
+            cv2.line(
+                img,
+                tuple(tag.corners[idx - 1, :].astype(int)),
+                tuple(tag.corners[idx, :].astype(int)),
+                (0, 255, 0),
+                thickness=10,
+            )
+
+    return img
